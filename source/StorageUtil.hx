7630d11ffd03b59ff8118bed4099739e800f653d
Ethantobot <ethantobot7@gmail.com>
2026-06-10 15:26:36 +0200

    Update StorageUtil.hx

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -15,6 +15,18 @@ import sys.FileSystem;
 import sys.io.File;
 #end
 
+#if android
+import android.Tools as AndroidTools;
+import android.Settings as AndroidSettings;
+import android.widget.Toast as AndroidToast;
+import android.content.Context as AndroidContext;
+import android.Permissions as AndroidPermissions;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Environment as AndroidEnvironment;
+import android.os.BatteryManager as AndroidBatteryManager;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
+
 using StringTools;
 
 /**
