267c0b00c42c05aa7396cc65de60884d7e322004
Ethantobot <ethantobot7@gmail.com>
2026-07-10 01:59:46 +0200

    Update PsychOgmoLoader.hx

--- source/PsychOgmoLoader.hx
+++ source/PsychOgmoLoader.hx
@@ -1,5 +1,6 @@
 package;
 
+#if !wiiu
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import funk.PsychFile;
 import haxe.Json;
@@ -8,21 +9,18 @@ using flixel.addons.editors.ogmo.FlxOgmo3Loader;
 
 class PsychOgmoLoader extends FlxOgmo3Loader
 {
-    public function new(projectDataPath:String, levelDataPath:String)
-    {
-        // Use PsychFile to cleanly route around the iOS bundle pathing issues
-        final rawProjectText = PsychFile.getContent(projectDataPath);
-        final rawLevelText = PsychFile.getContent(levelDataPath);
+	public function new(projectDataPath:String, levelDataPath:String)
+	{
+		final rawProjectText = PsychFile.getContent(projectDataPath);
+		final rawLevelText = PsychFile.getContent(levelDataPath);
 
-        // De-serialize the raw text into standard Haxe structures
-        final parsedProject:ProjectData = cast Json.parse(rawProjectText);
-        final parsedLevel:LevelData = cast Json.parse(rawLevelText);
+		final parsedProject:ProjectData = cast Json.parse(rawProjectText);
+		final parsedLevel:LevelData = cast Json.parse(rawLevelText);
 
-        // Trick Haxe into bypassing the traditional constructor layout
-        super(projectDataPath, levelDataPath);
+		super(projectDataPath, levelDataPath);
 
-        // Inject our safely managed runtime values directly into the fields
-        @:privateAccess this.project = parsedProject;
-        @:privateAccess this.level = parsedLevel;
-    }
+		@:privateAccess this.project = parsedProject;
+		@:privateAccess this.level = parsedLevel;
+	}
 }
+#end
