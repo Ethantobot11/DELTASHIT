8a0b21da35e4f5285408ca70795df8104cb2e324
Ethantobot <ethantobot7@gmail.com>
2026-07-10 01:32:20 +0200

    Refactor MenuState for WiiU compatibility

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,19 +1,31 @@
 package;
+
+#if wiiu
+import leafy.core.LeafyState;
+import leafy.core.LeafyG;
+import leafy.text.LeafyText;
+#else
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
+#end
 
-class MenuState extends FlxState
+class MenuState extends #if wiiu LeafyState #else FlxState #end
 {
 	public static var shittaleversion:String = 'DELTARUNE';
+	
+	#if !wiiu
 	var playButton:FlxButton;
 	var creditButton:FlxButton;
 	var titleText:FlxText;
 	var optionsButton:FlxButton;
 	#if (desktop || mobile)
 	var exitButton:FlxButton;
+	#end
+	#else
+	var titleText:LeafyText;
 	#end
 
 	override public function create()
@@ -22,9 +34,16 @@ class MenuState extends FlxState
 		DiscordClient.changePresence("WHOA SINCE WHEN THERE A MENU AND DISCORD RPC HERE ?", null);
 		#end
 
+		#if wiiu
+		var text = new LeafyText(10, 10, 100, "DELTARUNE : CHAPTER 7");
+		add(text);
+
+		titleText = new LeafyText(20, 0, 0, "HaxeFlixel\nTutorial\nGame", 22);
+		add(titleText);
+		#else
 		if (FlxG.sound.music == null)
 		{
-		FlxG.sound.playMusic(AssetPaths.AUDIO_STORY__ogg, 1, true);
+			FlxG.sound.playMusic(AssetPaths.AUDIO_STORY__ogg, 1, true);
 		}
 		var text = new flixel.text.FlxText(10, 10, 100, "DELTARUNE : CHAPTER 7");
 		add(text);
@@ -47,7 +66,7 @@ class MenuState extends FlxState
 		creditButton = new FlxButton(300, 0, "Credit", creditSwitch);
 		creditButton.x = (FlxG.width / 2) + -100;
 		creditButton.y = FlxG.height - creditButton.height - 10;
-    	add(creditButton);
+		add(creditButton);
 
 		#if (desktop || mobile)
 		exitButton = new FlxButton(FlxG.width - 28, 8, "X", clickExit);
@@ -55,56 +74,67 @@ class MenuState extends FlxState
 		add(exitButton);
 		#end
 
-        playButton.screenCenter();
-		//creditButton.screenCenter();
+		playButton.screenCenter();
 		text.screenCenter();
 		text.x = 30;
 		text.y = 60;
+		#end
+
 		super.create();
 	}
 
 	function clickPlay()
 	{
-	if (FlxG.sound.music != null)
-    {
-        FlxG.sound.music.stop();
-    }
-	//FlxG.switchState(PlayState.new);
-    FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
-	{
-	FlxG.switchState(new PlayState());
-	});
-    }
+		#if wiiu
+		LeafyG.switchState(new PlayState());
+		#else
+		if (FlxG.sound.music != null)
+		{
+			FlxG.sound.music.stop();
+		}
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
+		{
+			FlxG.switchState(new PlayState());
+		});
+		#end
+	}
 
 	function creditSwitch()
-    {
-	//FlxG.switchState(CreditState.new);
-	FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
-    FlxG.switchState(new CreditState());
-    }
+	{
+		#if wiiu
+		LeafyG.switchState(new CreditState());
+		#else
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
+		FlxG.switchState(new CreditState());
+		#end
+	}
 
 	function clickOptions()
 	{
-	FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
-	//FlxG.switchState(OptionsState.new);
-	FlxG.switchState(new OptionsState());
+		#if wiiu
+		LeafyG.switchState(new OptionsState());
+		#else
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
+		FlxG.switchState(new OptionsState());
+		#end
 	}
 
 	#if (desktop || mobile)
 	function clickExit()
 	{
-	#if DISCORD_ALLOWED
-	DiscordClient.shutdown();
-	trace("turned off rpc ?");
-	#end
-	Sys.exit(0);
+		#if DISCORD_ALLOWED
+		DiscordClient.shutdown();
+		trace("turned off rpc ?");
+		#end
+		Sys.exit(0);
 	}
 	#end
 
-	
-
 	override public function update(elapsed:Float)
 	{
+		#if wiiu
+		(WiiUGamepad.justPressed(A)) clickPlay();
+		#end
 		super.update(elapsed);
 	}
 }
